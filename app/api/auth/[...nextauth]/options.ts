01df55acb6d803eb98a9b4266503153983be9bf7
MagicSakuraD <ckgoforit@outlook.com>
2023-10-16 15:00:44 +0800

    add route.ts

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,31 @@
+import type { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "你的名字" },
+        password: { label: "Password", type: "password", placeholder: "密码" },
+      },
+      async authorize(credentials) {
+        const user = { id: "42", name: "test", password: "test" };
+
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
